877fbf033a00d14b34c47347b3895762f543debb
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:53 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.lars',
-    version='0.2',
+    version='0.2.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
-    description='An implementation of Least Angle Regression',
-    long_description=open('README.md').read(),
+    description='A naive implementation of Least Angle Regression',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
     license='MIT',
+    url='http://github.com/lmjohns3/py-lars',
     keywords=('regression '
               'sparse '
               'regularized '
               ),
-    url='http://github.com/lmjohns3/py-lars',
+    install_requires=['numpy'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
